81168d6ffdaaa1f982f149bfd265f86f2dcfbba8
Bruno Polaco <bpenteado@igcorp.com.br>
2014-01-17 12:07:20 -0200

    Method for discovering endianess of the file

--- groovedump.py
+++ groovedump.py
@@ -4,6 +4,11 @@
 import sys
 import struct
 
+
+LITTLE_ENDIAN_STRUCT_FORMAT = '<'
+BIG_ENDIAN_STRUCT_FORMAT = '>'
+
+
 class fileWithStruct(file):
 
     def read_unpack(self, format):
@@ -12,6 +17,19 @@ class fileWithStruct(file):
         by the format variable. See struct.unpack for the formatting options
         """
         return struct.unpack(format, self.read(struct.calcsize(format)))
+
+
+def discover_endianness(f):
+    big_endian_magic_number = (0xa1, 0xb2, 0xc3, 0xd4)
+
+    magic_number = f.read_unpack('BBBB')
+
+    if magic_number == big_endian_magic_number:
+        return BIG_ENDIAN_STRUCT_FORMAT
+    elif magic_number == big_endian_magic_number[::-1]:
+        return LITTLE_ENDIAN_STRUCT_FORMAT
+    else:
+        raise Exception('no magic number found')
 
 
 def main():
