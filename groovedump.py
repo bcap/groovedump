bca1a8d732868c81f4bb411387203de32b9700b1
Bruno Polaco <bpenteado@igcorp.com.br>
2014-01-17 11:34:13 -0200

    Extended file to enable reading and unpacking the data in one method

--- groovedump.py
+++ groovedump.py
@@ -2,6 +2,17 @@
 # -*- coding: UTF-8 -*-
 
 import sys
+import struct
+
+class fileWithStruct(file):
+
+    def read_unpack(self, format):
+        """
+        read the minimum amount of data from this file enough to unpack the data specified
+        by the format variable. See struct.unpack for the formatting options
+        """
+        return struct.unpack(format, self.read(struct.calcsize(format)))
+
 
 def main():
     pass
